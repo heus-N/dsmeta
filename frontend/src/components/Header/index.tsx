20fe2b7c15b317afbc970b89bc5ed7e983efa650
heus <1matheusnuness@gmail.com>
2022-07-15 00:11:55 -0300

    Other Components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import github from '../../assets/img/github.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img className="dsmeta-logo-container-img" src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por <img className="git-logo-white" src={github} alt="" />
+                    <a href="https://github.com/heus-N">/heus-N</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
